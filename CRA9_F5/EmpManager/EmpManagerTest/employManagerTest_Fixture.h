71ebb86fc4576f9d5492c56f492876b3f926729f
muk0920 <tkddn0928@naver.com>
2022-04-01 17:15:39 +0900

    [Feature] Add empty employManagerTest FixtureClass

---
+++ CRA9_F5/EmpManager/EmpManagerTest/employManagerTest_Fixture.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "pch.h"
+#include "gtest/gtest.h"
+#include "../EmpManager/employManager.h"
+#include "../EmpManager/employManager.cpp"
+
+class EmployManagerFixture : public ::testing::Test {
+
+protected:
+	void SetUp() override;
+	void TearDown()override;
+
+	EmployManager empManager;
+	vector<Employ> sampleEmpList;
+	vector<string> sampleOutList;
+
+	void makeSampleEmpList();
+	void makeSampleOutList();
+};
