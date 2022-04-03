074763e8e0689396ccddbbbacb166b2e5ce74dee
muk0920 <tkddn0928@naver.com>
2022-04-03 22:40:12 +0900

    [Feature] main 함수 기능 구현
    - argv[1] : input 텍스트 파일
    - argv[2] : output 텍스트 파일

--- CRA9_F5/EmpManager/EmpManager/main.cpp
+++ CRA9_F5/EmpManager/EmpManager/main.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
-#include "SampleClass.h"
+#include <fstream>
+#include <string>
+
+#include "employManager.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+	vector<string> ret;
+	string inputStr;
 
-	SampleClass tmp;
+	// parameter 유효성 검사
+	if (argc != 3)
+		exit(0);
 
-	cout << tmp.Add(1, 2);
+	if (strcmp(argv[1], "input.txt") != 0)
+		exit(0);
+
+	if (strcmp(argv[2], "output.txt") != 0)
+		exit(0);
+
+	ifstream inputFile(argv[1]);
+	if (!inputFile) {
+		cout << "ERROR, file is not exists" << endl;
+		exit(0);
+	}
+
+	ofstream outputFile(argv[2]);
+
+	while (getline(inputFile, inputStr)) {
+		EmployManager empManager;
+		ret = empManager.runCommand(inputStr);
+		for (auto & val : ret)
+			outputFile << val << endl;
+	}
+
+	inputFile.close();
+	outputFile.close();
 
 	return 0;
 }
