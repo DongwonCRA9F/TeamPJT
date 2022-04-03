9be7457b21723c3e2a7bb19cdfee2727c0d3381e
muk0920 <tkddn0928@naver.com>
2022-04-03 22:04:50 +0900

    [Feature] runCommand 함수 기능 구현
    - string 을 input으로 받아서, 각 command를 수행하고 vector<string> 을
    반환하는 함수

--- CRA9_F5/EmpManager/EmpManager/employManager.cpp
+++ CRA9_F5/EmpManager/EmpManager/employManager.cpp
@@ -5,7 +5,54 @@
 vector<string> EmployManager::runCommand(string input) {
 	vector<string> ret;
 
-	// TODO
+	// Parsing 작업
+	try {
+		if (!cmdParser.parsing("ADD,n, , ,01122329,DN WD,CL4,010-7174-5680,20071117,PRO", ",")) {
+			// Parsing 실패.
+		}
+	}
+	catch (const invalid_Command& e) {
+		cout << e.what() << endl;
+	}
+	catch (const invalid_Options& e) {
+		cout << e.what() << endl;
+	}
+
+	cmd = cmdParser.getCommand();
+	options = cmdParser.getOptions();
+	conditions = cmdParser.getConditions();
+
+	// Command 에 따라 cmdProcessor 호출
+	switch (cmd) {
+		case enumCommandList::ADD :
+			cmdProcessor = new CommandProcessorADD();
+			cmdResult = cmdProcessor->run(options, conditions);
+			break;
+		case enumCommandList::DEL :
+			cmdProcessor = new CommandProcessorDEL();
+			cmdResult = cmdProcessor->run(options, conditions);
+			if (options[0] == enumOptionList::None)
+				ret.push_back("DEL," + to_string(cmdResult.count));
+			else
+				ret = cmdResultTostrList("DEL", cmdResult);
+			break;
+		case enumCommandList::MOD :
+			cmdProcessor = new CommandProcessorMOD();
+			cmdResult = cmdProcessor->run(options, conditions);
+			if (options[0] == enumOptionList::None)
+				ret.push_back("MOD," + to_string(cmdResult.count));
+			else
+				ret = cmdResultTostrList("MOD", cmdResult);
+			break;
+		case enumCommandList::SCH :
+			cmdProcessor = new CommandProcessorSCH();
+			cmdResult = cmdProcessor->run(options, conditions);
+			if (options[0] == enumOptionList::None)
+				ret.push_back("SCH," + to_string(cmdResult.count));
+			else
+				ret = cmdResultTostrList("SCH", cmdResult);
+			break;
+	}
 
 	return ret;
 }
