afe17c5bf858eec74cfb14a076dbc4f320252f48
dongwon <bethewindz@naver.com>
2022-04-04 16:04:28 +0900

    Test Update test code based on singletone and gMock

--- CRA9_F5/EmpManager/EmpManagerTest/MockDatabase.h
+++ CRA9_F5/EmpManager/EmpManagerTest/MockDatabase.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "../EmpManager/DatabaseInterface.h"
 
-class MockDatabase : public DatabaseInterface {
+class MockDatabase {
 public:
-    MOCK_METHOD(bool, insertItem, (Employ employee), (override));
-    MOCK_METHOD(vector<Employ>, selectItems, (enumOptionList option, DatabaseSearchItem item), (override));
-    MOCK_METHOD(vector<Employ>, updateItems, (enumOptionList option, DatabaseSearchItem origin, DatabaseSearchItem update), (override));
-    MOCK_METHOD(vector<Employ>, deleteItems, (enumOptionList option, DatabaseSearchItem item), (override));
+    static MockDatabase& getInstance() {
+        static MockDatabase instance;
+        return instance;
+    }
+    MOCK_METHOD(bool, insertItem, (Employ employee));
+    MOCK_METHOD(vector<Employ>, selectItems, (enumOptionList option, DatabaseSearchItem item));
+    MOCK_METHOD(vector<Employ>, updateItems, (enumOptionList option, DatabaseSearchItem origin, DatabaseSearchItem update));
+    MOCK_METHOD(vector<Employ>, deleteItems, (enumOptionList option, DatabaseSearchItem item));
 };
