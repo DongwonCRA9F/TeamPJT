d8591e731ef5a1960d5ac5a6de16642a5c24a92d
eastson <eastson83@gmail.com>
2022-04-05 12:50:41 +0900

    [Feature] invalid input check

--- CRA9_F5/EmpManager/EmpManager/CommandProcessor.h
+++ CRA9_F5/EmpManager/EmpManager/CommandProcessor.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cctype>
+#include <sstream>
 #include "CommandResult.h"
 #include "CommandParser.h"
 #include "DatabaseInterface.h"
@@ -10,6 +12,83 @@ public:
 	CommandProcessor() {};
 	virtual CommandResult run(vector<enumOptionList> ResultOption, vector<string> ResultCondition) = 0;
 protected:
+	bool is_digit(string str) {
+		return atoi(str.c_str()) != 0 || str.compare("0") == 0;
+	}
+	bool IsValidEmployNum(string str) {
+		if (str.length() != 8) return false;
+		for (auto c : str) {
+			if (!isdigit(c)) return false;
+		}
+		int year = stoi(str.substr(0, 2));
+		if (year > 21 && year < 69) return false;
+
+		return true;
+	}
+	bool IsValidName(string str) {
+		int count_space = 0;
+		if (str.length() < 3 || str.length() > 15) return false;
+		for (auto c : str) {
+			if (!isalpha(c)) {
+				if (isspace(c)) count_space++;
+				else return false;
+			}			
+		}
+		if (count_space != 1) return false;
+		if (str.substr(0, 1) == " " || str.substr(str.length() - 1, 1) == " ") return false;
+		return true;
+	}
+	bool IsValidCl(string str) {
+		if (str.length() != 3) return false;
+		if (str.substr(0, 2) != "CL") return false;
+		if (!is_digit(str.substr(2, 1))) return false;
+		int cl_level = stoi(str.substr(2, 1));
+		if (cl_level == 0 || cl_level > 4) return false;
+		return true;
+	}
+	bool IsValidPhoneNum(string full) {
+		istringstream ss(full);
+		string str_buffer;
+		vector<string> phoneNum;
+		phoneNum.clear();
+
+		while (getline(ss, str_buffer, '-')) {
+			phoneNum.push_back(str_buffer);
+		}
+
+		if (phoneNum.size() != 3) return false;
+		for (string num_str : phoneNum) {
+			for (char c : num_str) {
+				if (!isdigit(c))
+					return false;
+			}
+		}
+
+		if (phoneNum[0] != "010") return false;
+		if (phoneNum[1].length() != 4) return false;
+		if (phoneNum[2].length() != 4) return false;
+
+		return true;
+	}
+	bool IsValidBirthday(string str) {
+		if (str.length() != 8) return false;
+		for (auto c : str) {
+			if (!isdigit(c)) return false;
+		}
+		int year = stoi(str.substr(0, 4));
+		int month = stoi(str.substr(4, 2));
+		int day = stoi(str.substr(6, 24));
+		if (year < 1900 || year > 2022) return false;
+		if (month < 1 || month > 12) return false;
+		if (day < 1 || day > 31) return false;
+
+		return true;
+	}
+	bool IsValidCerti(string str) {
+		if (str.length() < 2 || str.length() > 3) return false;
+		if (str!="ADV" && str != "PRO" && str != "EX") return false;
+		return true;
+	}
 	CommandResult result;
 };
 
@@ -18,6 +97,25 @@ class CommandProcessorADD :public CommandProcessor {
 public:
 	CommandProcessorADD() : CommandProcessor() {};
 	CommandResult run(vector<enumOptionList> ResultOption, vector<string> ResultCondition) override {
+		if(!IsValidEmployNum(ResultCondition[static_cast<int>(enumEmploy::EMPLOYEENUM)])) {
+			throw invalid_argument("[CommandProcessorADD] invalid_argument. (EMPLOYEENUM)\n");
+		}
+		if (!IsValidName(ResultCondition[static_cast<int>(enumEmploy::NAME)])) {
+			throw invalid_argument("[CommandProcessorADD] invalid_argument. (NAME)\n");
+		}
+		if (!IsValidCl(ResultCondition[static_cast<int>(enumEmploy::CL)])) {
+			throw invalid_argument("[CommandProcessorADD] invalid_argument. (CL)\n");
+		}
+		if (!IsValidPhoneNum(ResultCondition[static_cast<int>(enumEmploy::PHONENUM)])) {
+			throw invalid_argument("[CommandProcessorADD] invalid_argument. (PHONENUM)\n");
+		}
+		if (!IsValidBirthday(ResultCondition[static_cast<int>(enumEmploy::BIRTHDAY)])) {
+			throw invalid_argument("[CommandProcessorADD] invalid_argument. (BIRTHDAY)\n");
+		}
+		if (!IsValidCerti(ResultCondition[static_cast<int>(enumEmploy::CERTI)])) {
+			throw invalid_argument("[CommandProcessorADD] invalid_argument. (CERTI)\n");
+		}
+
 		if(DatabaseInterface::getInstance().insertItem(Employ(ResultCondition)))
 			result.count = 1;
 		else
