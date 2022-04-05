259aa6add87cce8f040ee16aaeddfb944e809dbb
muk0920 <tkddn0928@naver.com>
2022-04-05 20:56:12 +0900

    [Refactor] commandProcessor를 EmployManager 생성자에서 생성
    - enumComandList 를 Key로, commandProcessor* 를value로 하는  map 형태로
    관리하고, EmployManager 생성자에서 초기화 하는 작업 추가

--- CRA9_F5/EmpManager/EmpManager/employManager.h
+++ CRA9_F5/EmpManager/EmpManager/employManager.h
@@ -26,14 +26,22 @@ private:
 	CommandProcessor* cmdProcessor;
 	CommandResult cmdResult;
 
+	map<enumCommandList, CommandProcessor*> cmdProcessorArr;
+
 	vector<string> cmdList = { "ADD", "DEL", "SCH", "MOD" };
 	vector<string> clList = { "NONE", "CL1", "CL2", "CL3", "CL4" };
 	vector<string> certiList = { "ADV", "PRO", "EX" };
 
-
 	CommandProcessor* getProcessor(enumCommandList cmd);
 	string empNumTostr(int employNum);
 public:
+	EmployManager(){
+		cmdProcessorArr[enumCommandList::ADD] = new CommandProcessorADD<DatabaseInterface>();
+		cmdProcessorArr[enumCommandList::DEL] = new CommandProcessorDEL<DatabaseInterface>();
+		cmdProcessorArr[enumCommandList::MOD] = new CommandProcessorMOD<DatabaseInterface>();
+		cmdProcessorArr[enumCommandList::SCH] = new CommandProcessorSCH<DatabaseInterface>();
+	}
+
 	vector<string> runCommand(string input);
 	vector<string> cmdResultTostrList(enumCommandList cmd, CommandResult cmdResult);
 };
